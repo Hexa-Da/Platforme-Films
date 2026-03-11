f1e0fb65fa23082a0c3235ad6702cc35d25dbeb1
adrienlefebvre6 <adrien.lefebvre6@etu.univ-lorraine.fr>
2026-03-11 11:36:36 +0100

    étape 2 = modification movieController

--- src/main/java/edu/polytech/plateformefilms/controller/MovieController.java
+++ src/main/java/edu/polytech/plateformefilms/controller/MovieController.java
@@ -1,5 +1,6 @@
 package edu.polytech.plateformefilms.controller;
 
+import edu.polytech.plateformefilms.dto.MovieRequest;
 import edu.polytech.plateformefilms.model.Movie;
 import edu.polytech.plateformefilms.service.MovieService;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +18,55 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return ResponseEntity.ok(movieService.findAll());
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
         Movie movie = movieService.getById(id);
         return movie != null ? ResponseEntity.ok(movie) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String genre) {
+
+        if (title != null) return ResponseEntity.ok(movieService.searchByTitle(title));
+        if (genre != null) return ResponseEntity.ok(movieService.searchByGenre(genre));
+        return ResponseEntity.ok(movieService.findAll());
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody MovieRequest request) {
+        Movie movie = new Movie();
+        movie.setTitle(request.title());
+        movie.setDirector(request.director());
+        movie.setGenre(request.genre());
+        movie.setSynopsis(request.synopsis());
+        movie.setReleaseYear(request.releaseYear());
+
+        Movie created = movieService.createMovie(movie);
+
+        return ResponseEntity.status(201).body(created); // 201 = ressource créée
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody MovieRequest request) {
+        Movie updated = new Movie();
+        updated.setTitle(request.title());
+        updated.setDirector(request.director());
+        updated.setGenre(request.genre());
+        updated.setSynopsis(request.synopsis());
+        updated.setReleaseYear(request.releaseYear());
+
+        Movie newMovie = movieService.updateMovie(id, updated);
+
+        return ResponseEntity.ok(newMovie); // 200 : requête traitée
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build(); //noContent => renvoie 204 (traité mais rien à ne retouner)
     }
 }
